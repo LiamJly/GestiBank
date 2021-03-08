273f10ee0e851c4248eacb3d192289ad24457c03
LiamJly <joly.liam@gmail.com>
2021-03-08 18:22:54 +0100

    conversion

--- GestiBank/src/app/api.service.ts
+++ GestiBank/src/app/api.service.ts
@@ -6,10 +6,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiService {
 
-Api_url = "http://api.currencylayer.com/convert?access_key=b5811ef92e6c135ac6ae1598d91a5c6a"
+
   constructor(private httpClient: HttpClient) { }
 
-  /*getConversion(){
-    return this.httpClient.get(`${this.Api_url}`)
-  }*/
+  
 }
--- GestiBank/src/app/conversion-page/conversion-page.page.ts
+++ GestiBank/src/app/conversion-page/conversion-page.page.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conversion-page.page.scss'],
 })
 export class ConversionPagePage implements OnInit {
+private cur1: string;
+private cur2: string;
+private montant: number;
 
+res;
+Api_url = "https://free.currconv.com/api/v7/convert?apiKey=7cdd94e2a13de4cf4aff&compact=ultra&q="
   constructor() { }
 
   ngOnInit() {
-  }
 
+  }
+ getCurrency(){
+  fetch(`${this.Api_url}${this.cur1}_${this.cur2}`)
+  .then((res)=>{
+    return res.json();
+  })
+  .then(displaySum)
+  function displaySum(res){
+    console.log(res.USD_EUR)
+    console.log(res * this.montant)
+
+  }
+}
 }
