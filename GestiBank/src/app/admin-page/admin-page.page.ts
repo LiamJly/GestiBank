840ebe3f5ed4e24f6351e71a58c10b695ea795cc
Bedelo <s_ceb@hotmail.com>
2021-03-09 15:20:52 +0100

    adminpage redirection

--- GestiBank/src/app/admin-page/admin-page.page.ts
+++ GestiBank/src/app/admin-page/admin-page.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-page',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-page.page.scss'],
 })
 export class AdminPagePage implements OnInit {
+  constructor(private router : Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  toListAgent() {
+    this.router.navigate(['/list-agent']);
   }
-
 }
