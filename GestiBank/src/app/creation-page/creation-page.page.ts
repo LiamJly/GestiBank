c2310044c0681d5c2c01cddbf949c18385c6d2ec
Bedelo <s_ceb@hotmail.com>
2021-03-09 14:40:41 +0100

    admin page implementation

--- GestiBank/src/app/creation-page/creation-page.page.ts
+++ GestiBank/src/app/creation-page/creation-page.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 import { ClientService } from '../services/client.service';
 
 @Component({
@@ -7,33 +9,65 @@ import { ClientService } from '../services/client.service';
   styleUrls: ['./creation-page.page.scss'],
 })
 export class CreationPagePage implements OnInit {
-
-  constructor(private client : ClientService) { }
+  constructor(private client: ClientService, private router: Router, private toastController: ToastController) {}
   nom: string;
   prenom: string;
   email: string;
   tel: number;
   myClient;
+  validation;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.validation= "favorite"
+  }
 
-
-  envoyer(){
+  envoyer() {
+    this.validation = 'primary';
     this.myClient = {
       nom: this.nom,
       prenom: this.prenom,
       email: this.email,
       tel: this.tel,
-      role:"client",
+      role: 'client',
       status: 'en attente',
-      motpass: this.client.genPass(), //implementer un generateur
+      motpass: this.client.genPass(), 
     };
     
-    if(this.nom!="" && this.prenom!="" && this.email!="" && this.tel!=null  ){
-        console.log(this.myClient);
-        this.client.addClient(this.myClient).subscribe();
-    }else{
-        console.log("TOAST A IMPLEMENTER : REMPLIR TOUT LES CHAMPS!")
+    if (
+      this.nom != '' &&
+      this.prenom != '' &&
+      this.email != '' &&
+      this.tel != null
+    ) {
+      console.log(this.myClient);
+      this.client.addClient(this.myClient).subscribe();
+      this.presentToastOK()
+      this.toConnectPage();
+    } else {
+      this.presentToastNOK()
     }
+    this.validation="favorite"
+  }
+
+  toConnectPage() {
+    this.router.navigate(['/connexion-page']);
+  }
+
+  async presentToastOK() {
+    const toast = await this.toastController.create({
+      message: 'Vous etes inscrit, felicitation',
+      color: 'success',
+      duration: 2000,
+    });
+    toast.present();
+  }
+
+  async presentToastNOK() {
+    const toast = await this.toastController.create({
+      message: "Remplir tout les champs s'il vous plait!",
+      color: 'warning',
+      duration: 2000,
+    });
+    toast.present();
   }
 }
