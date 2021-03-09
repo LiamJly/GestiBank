0345287c5cf69bcca8d740ebd27919dd367be8a1
LiamJly <joly.liam@gmail.com>
2021-03-09 16:49:41 +0100

    list agent almost done, adding delete

--- GestiBank/src/app/list-agents/list-agents.page.ts
+++ GestiBank/src/app/list-agents/list-agents.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AgentService } from '../services/agent.service';
 
 @Component({
   selector: 'app-list-agents',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-agents.page.scss'],
 })
 export class ListAgentsPage implements OnInit {
-
-  constructor() { }
+mesAgents;
+  constructor(private agent: AgentService) { }
 
   ngOnInit() {
+    this.displayAgents(); 
   }
 
+  displayAgents(){
+    this.agent.getAgents().subscribe(
+      (res)=>{
+       // console.log(res);
+        this.mesAgents = res;
+        console.log(this.mesAgents);
+      }
+    )
+  }
 }
